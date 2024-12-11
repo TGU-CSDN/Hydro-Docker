55af667dd5fe37acd70baa46db4d867eaee158d2
fly3949 <i@iifly.cn>
2024-12-12 00:44:10 +0800

    fix: fix backend server config

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -13,8 +13,10 @@ fi
 if [ ! -f "$ROOT/.install.lock" ]; then
     echo "INSTALL LOCK FILE DO NOT REMOVE IT" > "$ROOT/.install.lock"
 
+    hydrooj cli system set server.host 0.0.0.0
     hydrooj cli system set server.xff x-forwarded-for
     hydrooj cli system set server.xhost x-forwarded-host
+    hydrooj cli system set server.xproxy true
 
     hydrooj cli user create systemjudge@systemjudge.local root rootroot 2
     hydrooj cli user setSuperAdmin 2
